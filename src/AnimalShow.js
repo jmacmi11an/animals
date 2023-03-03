55f0c28c1d9c55fe0e2453bf5c107ddca8e75c41
Jonathan C MacMillan <jmacmill@gmail.com>
2023-03-03 18:59:05 +1000

    Add CSS styling

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import bird from './svg/bird.svg'
-import cat from './svg/cat.svg'
-import cow from './svg/cow.svg'
-import dog from './svg/dog.svg'
-import gator from './svg/gator.svg'
-import heart from './svg/heart.svg'
-import horse from './svg/horse.svg'
+import './AnimalShow.css';
+import { useState } from 'react';
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
 
-const svgMap = {
+const svgMap = { 
     bird, 
     cat,
     cow,
@@ -22,9 +23,10 @@ function AnimalShow({type }) {
         setClicks(clicks +1);
     };
 
-    return <div onClick={handleClick}>
-            <img alt="animal" src={svgMap[type]} />
+    return <div className="animal-show" onClick={handleClick}>
+            <img className="animal" alt="animal" src={svgMap[type]} />
             <img 
+                className="heart"
                 alt="heart" 
                 src={heart} 
                 style={{ width: 10 + 10 * clicks + 'px',  }}
